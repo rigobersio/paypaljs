441f2f12e03cab499b4b21709aee31554d9a3b44
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-05-29 18:55:35 -0400

    (rizen)edit:App main(se limpia App y en main de envuelve en PayPalScriptProvier)

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PayPalScriptProvider>
+      <App />
+    </PayPalScriptProvider>
   </React.StrictMode>,
 )
